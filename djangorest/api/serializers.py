7d9e3d67d087f7c01b5709da08e80faaea010049
evantoh <evanmwenda@gmail.com>
2018-06-13 18:57:55 +0300

    create MerchSerializer class that inherits from the ModelSerializer class

--- djangorest/api/serializers.py
+++ djangorest/api/serializers.py
@@ -1,12 +1,8 @@
 from rest_framework import serializers
 
-from .models import Bucketlist
+from .models import MoringaMerch
 
-class BucketlistSerializer(serializers.ModelSerializer):
-    """Serializer to map the Model instance into JSON format."""
-
+class MerchSerializer(serializers.ModelSerializer):
     class Meta:
-        """Meta class to map serializer's fields with the model fields."""
-        model = Bucketlist
-        fields = ('id', 'name', 'date_created', 'date_modified')
-        read_only_fields = ('date_created', 'date_modified')
+        model = MoringaMerch
+        fields = ('name', 'description', 'price')
