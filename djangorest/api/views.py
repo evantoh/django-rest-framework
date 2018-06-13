a955fc7614697ff1a01545e224e31a5b722899af
evantoh <evanmwenda@gmail.com>
2018-06-13 18:59:42 +0300

    create an API view to handle the requests.

--- djangorest/api/views.py
+++ djangorest/api/views.py
@@ -1,14 +1,11 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import  MoringaMerch
+from .serializers import MerchSerializer
 
-from rest_framework import generics
-from .serializers import BucketlistSerializer
-from .models import Bucketlist
-
-class CreateView(generics.ListCreateAPIView):
-    """This class defines the create behavior of our rest api."""
-    queryset = Bucketlist.objects.all()
-    serializer_class = BucketlistSerializer
-
-    def perform_create(self, serializer):
-        """Save the post data when creating a new bucketlist."""
-        serializer.save()
+#........
+class MerchList(APIView):
+    def get(self, request, format=None):
+        all_merch = MoringaMerch.objects.all()
+        serializers = MerchSerializer(all_merch, many=True)
+        return Response(serializers.data)
